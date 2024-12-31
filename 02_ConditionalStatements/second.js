15b5e2a0042d8337a55c37752694c33b8f15c286
sahilghorpade27 <sahilpg27@gmail.com>
2024-12-31 20:15:09 +0530

    Done with the Conditional Statements and Operators

--- 02_ConditionalStatements/second.js
+++ 02_ConditionalStatements/second.js
@@ -1,1 +1,40 @@
+// Comment 
+/* 
+ duble line comments
+ */
+
+//Unary Operators 
+
+let a = 3 ;
+let b= 4 ;
+
+console.log("a= ",++a);
+console.log("a= ",a++);
+console.log("a= ",a);
+
+let m = 1 ;
+let n = "1"
+console.log(m==n);
+console.log(m===n);
+
+//Conditional Statements 
+/* if(){
+    }
+   else{
+    
+   }
+
+
+while(){
+
+  }
+
+*/
+
+//Ternary 
+
+let age = 19 ;
+
+let res = age >18 ? "adult" : "no adult" ;
+console.log(res);
 
