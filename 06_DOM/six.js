fd0f62833505ae185ae2787c65a0dcda3618714f
sahilghorpade27 <sahilpg27@gmail.com>
2025-01-04 00:39:34 +0530

    completed with first part of DOM

---
+++ 06_DOM/six.js
@@ -0,0 +1,42 @@
+// Get element by ID 
+
+/*
+let heading = document.getElementById("heading") ;
+
+heading.innerText = "Hello" ;
+
+*/
+
+
+// Get element by class Name 
+// let headingclass = document.getElementsByClassName("heading2");
+
+ // return HTML collections in the form of array 
+
+
+// Get element by Tag name 
+
+/*
+
+let parahs = document.getElementsByTagName("p");
+console.log(parahs);
+
+*/
+
+//Query Selector 
+
+/*
+let firstel = document.querySelector("p");     //gives firest element
+console.dir(firstel); 
+
+let sectel = document.querySelector("p");     //gives all elements of P tag 
+console.dir(secel); 
+*/
+
+
+//Practice
+let h= document.querySelector(".apna");
+h.innerText = h.innerText+" from Apnacollege to students"
+
+
+
