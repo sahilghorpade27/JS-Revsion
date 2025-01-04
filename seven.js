a65b9b7695c56c4a3a9a1ac3e68c8ef20d723049
sahilghorpade27 <sahilpg27@gmail.com>
2025-01-04 11:52:11 +0530

    done with the dom elements and all things

--- seven.js
+++ seven.js
@@ -1,5 +1,73 @@
+
+//Getting attribute 
+
+/*
+
 let div = document.querySelector("div");
 console.log(div);
 
 let id = div.getAttribute("id");
 console.log(id);
+
+*/
+
+//Setting the Attribute 
+
+/*
+let div = document.querySelector("div");
+div.setAttribute("class","newclass");
+console.log(div.getAttribute("class"));
+
+*/
+
+
+//Style 
+
+/*
+let div = document.querySelector("div");
+div.style.backgroundColor = "yellow"
+div.style.color = "Red";
+*/
+
+
+////////////////////Insert Elements
+
+
+//created button 
+
+/*
+let newbtn = document .createElement("button");
+newbtn.innerText = "Click me to say hello" ;
+newbtn.style.backgroundColor = "blue" ;
+newbtn.style.color = "white";
+console.log(newbtn);
+
+let div = document.querySelector("div");
+div.append(newbtn);
+div.before(newbtn);
+div.after(newbtn);
+div.prepend(newbtn);
+
+*/
+
+
+
+//created heading
+
+/*
+let newhead = document.createElement("h1");
+newhead.innerText = "Hello I am new Heading" ;
+newhead.style.color = "Blue";
+
+document.querySelector("body").before(newhead);
+
+*/
+
+
+//remove node
+/*
+
+newhead.remove();
+newbtn.remove();
+
+*/
